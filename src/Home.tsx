9087046ade0c23a93da49a1338ecf84baeb80a55
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-01-08 18:06:13 +0530

    linked

--- src/Home.tsx
+++ src/Home.tsx
@@ -95,7 +95,7 @@ function Home() {
           <br></br>
           <br></br>
          {Capacitor.getPlatform()=="web"
- &&  <a className="white"  href="https://dl.dropbox.com/scl/fi/gw6fm9gyby3wtd8vzxb1o/Omni-files.apk?rlkey=wnutrbgjbxzhc6hm81a3x7byf&dl=0" ><h4>Get the android app HERE!</h4></a>
+ &&  <a className="white"  href="https://dl.dropbox.com/scl/fi/90pbzilpwnamii3iy85ej/Omni-files.apk?rlkey=baywstnl3iqj7nh7471l33ns6&dl=0" ><h4>Get the android app HERE!</h4></a>
          }
         </div>
       </div>
